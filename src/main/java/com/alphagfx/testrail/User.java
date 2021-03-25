cfc5e003ce24271c1d5287bfe57e43ec417b7f3b
Uladzislau Makarevich <vgarbat@gmail.com>
2021-03-26 02:47:42 +0300

    feat: Add Smart decorator for User

--- src/main/java/com/alphagfx/testrail/User.java
+++ src/main/java/com/alphagfx/testrail/User.java
@@ -1,9 +1,58 @@
 package com.alphagfx.testrail;
 
+import org.json.JSONObject;
+
 public interface User extends JsonReadable {
 
 	TestRail testrail();
 
 
 	int id();
+
+
+	class Smart implements User {
+
+		private final User user;
+
+
+		public Smart(User user) {
+			this.user = user;
+		}
+
+
+		@Override
+		public TestRail testrail() {
+			return user.testrail();
+		}
+
+		@Override
+		public int id() {
+			return user.id();
+		}
+
+		public String email() {
+			return json().getString("email");
+		}
+
+		public boolean isActive() {
+			return json().getBoolean("is_active");
+		}
+
+		public String name() {
+			return json().getString("name");
+		}
+
+		public int roleId() {
+			return json().getInt("role_id");
+		}
+
+		public String role() {
+			return json().getString("role");
+		}
+
+		@Override
+		public JSONObject json() {
+			return user.json();
+		}
+	}
 }
--- src/main/java/com/alphagfx/testrail/Users.java
+++ src/main/java/com/alphagfx/testrail/Users.java
@@ -4,5 +4,8 @@ public interface Users {
 
 	User get(int id);
 
-	Iterable<User> list();
+	// TODO: 3/26/21 Add get_current_user API method
+	// TODO: 3/26/21 ? Add get_user_by_email ?
+
+	Iterable<User> list(/* TODO: 3/26/21 project_id filter may be here */);
 }
